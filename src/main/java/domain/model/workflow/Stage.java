1dc81fde792bb1866e1c6b84c1805c17c4dfe9f6
hua111487 <hua111487@gmail.com>
2020-04-21 17:09:39 +0800

    merge master

--- src/main/java/domain/model/workflow/Stage.java
+++ src/main/java/domain/model/workflow/Stage.java
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-package domain.model.workflow;
-
-import domain.model.card.Card;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-public class Stage {
-    private String stageName;
-    private String stageId;
-
-    public Stage(String stageName) {
-        this.stageName = stageName;
-        stageId = "S" + UUID.randomUUID().toString();
-    }
-
-    public String getStageId() {
-        return stageId;
-    }
-}
-=======
 package domain.model.workflow;
 
 import java.util.HashMap;
@@ -58,4 +35,3 @@ public class Stage implements Lane {
 //
 //    }
 }
->>>>>>> e8e9c1737fc80ce58f42cc8cdd31372fd6bbbb19
