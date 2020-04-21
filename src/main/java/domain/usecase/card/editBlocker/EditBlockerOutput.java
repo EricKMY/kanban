9f1d9f250ef300a41571d733e3e9e7a2f1f6d2d1
Stanley Zhong <ss19971030@gmail.com>
2020-04-21 14:58:54 +0800

    fix edit blocker

--- src/main/java/domain/usecase/card/editBlocker/EditBlockerOutput.java
+++ src/main/java/domain/usecase/card/editBlocker/EditBlockerOutput.java
@@ -1,13 +1,4 @@
 package domain.usecase.card.editBlocker;
 
 public class EditBlockerOutput {
-    private String blockerDescription;
-
-    public void setBlocker(String blocker) {
-        this.blockerDescription = blocker;
-    }
-
-    public String getBlocker() {
-        return blockerDescription;
-    }
 }
--- src/test/java/domain/usecase/card/moveCard/MoveCardUseCaseTest.java
+++ src/test/java/domain/usecase/card/moveCard/MoveCardUseCaseTest.java
@@ -1,4 +1,44 @@
 package domain.usecase.card.moveCard;
 
+import domain.adapter.card.CardRepository;
+import domain.model.card.Card;
+import domain.usecase.card.createCard.CreateCardInput;
+import domain.usecase.card.createCard.CreateCardOutput;
+import domain.usecase.card.createCard.CreateCardUseCase;
+import domain.usecase.card.editBlocker.EditBlockerInput;
+import domain.usecase.card.editBlocker.EditBlockerOutput;
+import domain.usecase.card.editBlocker.EditBlockerUseCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class MoveCardUseCaseTest {
+    private CardRepository cardRepository;
+    private Card card;
+
+    @Before
+    public void setup() {
+        cardRepository = new CardRepository();
+        CreateCardUseCase createCardUseCase = new CreateCardUseCase(cardRepository);
+        CreateCardInput input = new CreateCardInput();
+        CreateCardOutput output = new CreateCardOutput();
+
+        input.setCardName("Design domain model");
+
+        createCardUseCase.execute(input, output);
+        String cardId = output.getCardId();
+
+        card = cardRepository.findById(cardId);
+        assertNotNull(card);
+    }
+
+    @Test
+    public void moveCardWithoutBlocker() {
+    }
+
+    @Test
+    public void moveCardWithBlocker() {
+    }
 }
