a634a663eaeaca4da47ed2eb4c1074361f0716ee
Stanley Zhong <ss19971030@gmail.com>
2020-04-22 17:27:24 +0800

    revise create stage and swinlane

---
+++ src/main/java/domain/usecase/workflow/lane/CreateSwinlaneOutput.java
@@ -0,0 +1,13 @@
+package domain.usecase.workflow.lane;
+
+public class CreateSwinlaneOutput {
+    private String swinlaneId;
+
+    public String getSwinlaneId() {
+        return swinlaneId;
+    }
+
+    public void setSwinlaneId(String swinlaneId) {
+        this.swinlaneId = swinlaneId;
+    }
+}
